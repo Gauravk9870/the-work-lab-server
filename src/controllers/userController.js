d6230d0ef1e193705b07fa750ffe08393b1d11b3
TrippyGaurav <141822413+TrippyGaurav@users.noreply.github.com>
2024-01-21 19:52:13 +0530

    add api for single freelancer

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -74,7 +74,7 @@ const registerUser = asyncHanlder(async (req, res) => {
   });
 
   if (user) {
-   return res.status(200).json({
+    return res.status(200).json({
       _id: user.id,
       firstName: user.firstName,
       lastName: user.lastName,
@@ -91,21 +91,21 @@ const loginUser = asyncHanlder(async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(201).json({error:"You are not registered"});
+    return res.status(201).json({ error: "You are not registered" });
   }
 
   // Check for the user email
   const user = await Freelancer.findOne({ email });
 
   if (user && (await bcrypt.compare(password, user.password))) {
-   return res.status(201).json({
+    return res.status(201).json({
       _id: user.id,
       name: `${user.firstName} ${user.lastName}`,
       email: user.email,
       token: generateToken(user._id)
     });
   } else {
-   return res.status(201).json({});
+    return res.status(201).json({});
   }
 })
 
@@ -120,6 +120,26 @@ const getUsers = asyncHanlder(async (req, res) => {
 
 })
 
+const getUserById = asyncHanlder(async (req, res) => {
+  const userId = req.params.id;
+
+  console.log("ID : ", userId);
+  try {
+    const user = await Freelancer.findById(userId);
+
+    if (user) {
+      console.log(user);
+      return res.status(200).json(user);
+    } else {
+      console.log(user);
+
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong, Please try again" });
+  }
+})
+
 module.exports = {
-  registerUser, loginUser, getUsers
+  registerUser, loginUser, getUsers, getUserById
 };
--- src/routes/freelancerRoutes.js
+++ src/routes/freelancerRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUsers } = require("../controllers/userController");
+const { registerUser, loginUser, getUsers, getUserById } = require("../controllers/userController");
 
 router.get("/", getUsers);
+router.get("/:id", getUserById);
 router.post("/register", registerUser);
 router.post('/login', loginUser);
 
